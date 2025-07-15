1c78e3736fa36015b4a5028d13e160fbaf9ef442
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2025-07-15 15:35:27 +0300

    chore: migrate ESLint configuration to new format and update linting scripts

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslintRecommended from 'eslint/conf/eslint-recommended.js';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  eslintRecommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  prettierConfig,
+  {
+    files: ['**/*.ts'],
+    ignores: ['dist', 'node_modules', '*.js'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+    },
+  },
+];
